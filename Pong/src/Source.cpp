dc107fc830ba0248fe2946caf31d006956b9617a
ancient-vinegar <55340290+ancient-vinegar@users.noreply.github.com>
2022-05-22 18:57:59 -0400

    VS Project Setup

---
+++ Pong/src/Source.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+int main() {
+
+	std::cout << "Hello" << std::endl;
+
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -2;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
